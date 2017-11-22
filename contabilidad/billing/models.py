46af16ff9d1a249195b4531c3ca248635a49c9f2
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-11-22 14:02:47 -0600

    Add billing model

---
+++ contabilidad/billing/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.contrib.auth.models import User
+
+
+class TicketParser(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=20)
+
+
+class Store(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=250)
+    parser = models.ForeignKey(TicketParser, null=False, related_name='store')
+
+
+class TicketsImage(models.Model):
+    image = models.ImageField(null=False, upload_to='billing/tickets')
+
+
+class Ticket(models.Model):
+    store = models.ForeignKey(Store, null=False, related_name='ticket')
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+    created_by = models.ForeignKey(User, null=False, related_name="tickets_created")
+    updated_by = models.ForeignKey(User, null=True, related_name="tickets_updated")
+    data = JSONField(null=False, default={})
+
+    def save(self, *args, **kwargs):
+        user = None
+        if self.pk is None:
+            self.created_by = user
+        else:
+            self.updated_by = user
+        super(Ticket, self).save(*args, **kwargs)
