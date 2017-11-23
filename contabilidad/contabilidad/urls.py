8728e965321c8c9d2e054918d993f7729ed8696a
Ricardo Tercero Solis <ricardo.tercero.solis@gmail.com>
2017-11-22 19:52:34 -0600

    Update user fixture

--- contabilidad/contabilidad/urls.py
+++ contabilidad/contabilidad/urls.py
@@ -6,6 +6,6 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title=_('Accounting API'))
 
 urlpatterns = [
-    url(r'^', schema_view),
     url(r'^admin/', admin.site.urls),
+    url(r'^', schema_view),
 ]
