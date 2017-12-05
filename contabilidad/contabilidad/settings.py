e6dadc25346c6be71ef86093ea5a17fe6b41e6d2
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-12-04 22:14:05 -0600

    Add storages app

--- contabilidad/contabilidad/settings.py
+++ contabilidad/contabilidad/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
