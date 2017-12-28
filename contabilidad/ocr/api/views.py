0cc7d7cb1678fed5ecf4716856027d6931f26fd7
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-12-28 09:28:02 -0600

    Filter OCR end point to display only your requests if you are not a admin or super user other wise display all

--- contabilidad/ocr/api/views.py
+++ contabilidad/ocr/api/views.py
@@ -7,3 +7,10 @@ class OCRRequestViewSet(viewsets.ModelViewSet):
     queryset = OCRRequest.objects.all()
     serializer_class = OCRRequestSerializer
     http_method_names = ['get', 'post']
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OCRRequestViewSet, self).get_queryset(*args, **kwargs)
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return qs
+        return qs.filter(created_by = user)
