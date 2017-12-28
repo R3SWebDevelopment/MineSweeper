2dd17b35172781637ea8a7e781fdfe940df6d422
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2017-12-27 23:05:43 -0600

    Improve OCR serializer

--- contabilidad/ocr/api/serializers.py
+++ contabilidad/ocr/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from ..models import OCRRequest
+from utils.api.serializers import UserSerializer
 
 
 class OCRRequestSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
+    updated_by = UserSerializer(read_only=True)
+    working_path = serializers.CharField(read_only=True)
+    result = serializers.CharField(read_only=True)
+    log = serializers.JSONField(read_only=True)
 
     class Meta:
         model = OCRRequest
