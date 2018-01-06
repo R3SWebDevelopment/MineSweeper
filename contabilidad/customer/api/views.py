a11c37e553e6cd95ba577a50590da530f7ab83a1
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-01-06 16:20:12 -0600

    Add end point to add collaborator to company

--- contabilidad/customer/api/views.py
+++ contabilidad/customer/api/views.py
@@ -1,16 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
-from .serializers import CompanySerializer, Company
+from .serializers import CompanySerializer, Company, AddCollaboratorsSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from .permissions import AdminAndCollaboratorPermission
 from rest_framework.permissions import IsAuthenticated
+from django.utils.translation import ugettext as _
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post', 'patch']
     permission_classes = (AdminAndCollaboratorPermission, IsAuthenticated)
 
     def get_queryset(self, *args, **kwargs):
@@ -20,7 +21,17 @@ class CompanyViewSet(viewsets.ModelViewSet):
         return user.companies.all()
 
     @detail_route(methods=['post'])
-    def add_collaborators(self, request, pk):
+    def add_collaborators(self, request, *args, **kwargs):
         company = self.get_object()
-        return Response({}, status=status.HTTP_403_FORBIDDEN)
+        data_serializer = AddCollaboratorsSerializer(request.data)
+        new_collaborators = data_serializer.get_users().\
+            exclude(pk__in=company.collaborators.all().values_list('id', flat=True))
+
+        if new_collaborators.exists():
+            company.collaborators.add(*list(new_collaborators))
+            object_serializer = self.get_serializer_class()(company)
+            return Response(object_serializer.data, status=status.HTTP_200_OK)
+        return Response({
+            "message": _("No user selected nor is already a collaborator nor valid user")
+        }, status=status.HTTP_403_FORBIDDEN)
 
