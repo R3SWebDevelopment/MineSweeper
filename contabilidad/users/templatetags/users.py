f245fa946ab391d5c2f3652db17c55ecb6500de3
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-01-08 22:32:26 -0600

    Update email registration notification

--- contabilidad/users/templatetags/users.py
+++ contabilidad/users/templatetags/users.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 register = template.Library()
 
 
-@register.filter(name='get_user')
+@register.simple_tag
 def get_user(email):
     return User.objects.filter(email__iexact=email).first()
