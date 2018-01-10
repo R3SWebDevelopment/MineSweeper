f105158b83cfa1a187155f5907abe2651ca4fdc2
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-01-10 09:10:52 -0600

    Add method to create notification

--- contabilidad/notifications/models.py
+++ contabilidad/notifications/models.py
@@ -1,5 +1,18 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField, JSONField
+import boto3
+from django.conf import settings
+
+SNS_ENABLED = settings.SNS_ENABLED or False
+SNS_ACCESS_KEY = settings.SNS_ACCESS_KEY or None
+SNS_SECRET_ACCESS_KEY = settings.SNS_SECRET_ACCESS_KEY or None
+SNS_REGION_NAME = settings.SNS_REGION_NAME or None
+
+
+def is_sms_enable():
+    if SNS_ENABLED and SNS_ACCESS_KEY is not None and SNS_SECRET_ACCESS_KEY is not None and SNS_REGION_NAME is not None:
+        return True
+    return False
 
 
 class Notification(models.Model):
@@ -14,10 +27,10 @@ class Notification(models.Model):
 
     email_subject = models.CharField(max_length=250, null=True)
     email_message = models.TextField()
-    email_send = models.BooleanField(default=False)
-    email_has_error = models.BooleanField(default=False)
-    email_response = models.TextField()
-    email_error = models.TextField()
+    email_send = JSONField()
+    email_has_error = JSONField()
+    email_response = JSONField()
+    email_error = JSONField()
 
     sms = models.BooleanField(default=False)
 
@@ -27,7 +40,49 @@ class Notification(models.Model):
 
     sms_subject = models.CharField(max_length=250, null=True)
     sms_message = models.TextField()
-    sms_send = models.BooleanField(default=False)
-    sms_has_error = models.BooleanField(default=False)
-    sms_response = models.TextField()
+    sms_send = JSONField()
+    sms_has_error = JSONField()
+    sms_response = JSONField()
     sms_error = models.TextField()
+
+    @classmethod
+    def notify(cls, users, email_subject, email_message, sms_subject, sms_message, sms_override=False,
+               email_override=False):
+        numbers = []
+        mails = []
+
+        if not sms_override:
+
+            if is_sms_enable() and users is not None:
+                numbers_list = list(users.objects.filter(profile__notify_by_email=True).
+                                    values_list('profile__mobile_number', flat=True))
+
+                for num in numbers_list:
+                    number = "{}{}".format(num.get('country_code', ''), num.get('number', ''))
+                    if number.strip():
+                        numbers.append(number)
+
+        if not email_override:
+            mails = list(users.objects.filter(profile__notify_by_sms=True).values_list('email', flat=True))
+
+        if len(numbers) > 0 or len(mails) > 0:
+            send_mail = len(mails) > 0
+            send_sms = len(numbers) > 0
+            cls.objects.create(email=send_mail, sms=send_sms, email_subject=email_subject, email_message=email_message,
+                               sms_subject=sms_subject, sms_message=sms_message)
+
+    def _send_email(self):
+        pass
+
+    def _send_sms(self):
+        try:
+            aws_access_key_id = None
+            aws_secret_access_key = None
+            region_name = None
+
+            client = boto3.client('sns', aws_access_key_id=aws_access_key_id,
+                                  aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+
+            response = client.publish(PhoneNumber='+528110713920', Message='Testing TEXT', Subject='OTRA')
+        except Exception as e:
+            pass
