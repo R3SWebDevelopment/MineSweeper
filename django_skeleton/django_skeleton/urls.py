90489cd5d45eda199de0227641cf6f74b4fb53e1
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-05-09 18:04:03 -0500

    Update url to just enable game board url

--- django_skeleton/django_skeleton/urls.py
+++ django_skeleton/django_skeleton/urls.py
@@ -12,16 +12,9 @@ schema_view = get_swagger_view(title=_('Django Skeleton'))
 
 urlpatterns = [
     url("^admin/", admin.site.urls),
-    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^accounts/', include('allauth.urls')),
     url(r'^users/', include('users.api.urls', namespace='users_api')),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^system/', include('utils.api.urls', namespace='system_api')),
     url(r'^mineswipper/', include('mine_swipper.api.urls', namespace='mine_swipper')),
     url(r'^$', schema_view),
-    url(r'^avatar/', include('avatar.urls')),
 ]
 
 if settings.ENABLE_DEBUG_TOOLBAR:
