6f6a74c7da1c080e37d7318bbcd954758ab92df9
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-05-08 22:44:55 -0500

    Fix celery port

--- django_skeleton/django_skeleton/settings.py
+++ django_skeleton/django_skeleton/settings.py
@@ -333,7 +333,7 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
-REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6378/0')
+REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 
 USE_CELERY = get_bool_env('USE_CELERY', True)
 if USE_CELERY:
