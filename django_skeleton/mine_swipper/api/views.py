5f82435fd0a72b5450c4dc8e283529ed0ef3025f
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-05-09 23:23:57 -0500

    Change permission class for game end point

--- django_skeleton/mine_swipper/api/views.py
+++ django_skeleton/mine_swipper/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from .serializers import GameSerializer, GameInputSerializer, GameStatusSerializer
 from ..models import Game
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from crum import get_current_user
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 class GameViewSet(viewsets.ModelViewSet):
     serializer_class = GameSerializer
     queryset = Game.objects.all()
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     http_method_names = ['get', 'post']
 
     def get_serializer_class(self):
