e3b9ad99f30ebe67649596f96e8146ee18117a98
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-05-10 13:10:10 -0500

    Valid that input are correct

--- React/mineswipper/src/components/signin.js
+++ React/mineswipper/src/components/signin.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '../css/signin.css';
+import { onChangeInput } from '../utils/inputs';
 
 
 class SignIn extends Component{
@@ -9,8 +10,20 @@ class SignIn extends Component{
   constructor(props){
     super(props)
     this.state = {
-      username: '',
-      password: ''
+      email: '',
+    }
+  }
+
+  onChangeInput = (evt) => {
+    onChangeInput(evt, this);
+  }
+
+  submit = (evt) => {
+    evt.preventDefault();
+    if(this.state.email.trim().length === 0){
+      alert("You need to input your email address");
+    }else{
+      console.log('GOING')
     }
   }
 
@@ -23,10 +36,12 @@ class SignIn extends Component{
                   <h1 className="h3 mb-3 font-weight-normal text-center">
                       Please Access Game
                   </h1>
-                  <label for="inputEmail" className="sr-only">Email address</label>
-                  <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-                  <br />
-                  <button className="btn btn-lg btn-primary btn-block" type="submit">Access Game</button>
+                  <form onSubmit={this.submit.bind(this)}>
+                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="email" name="email" className="form-control" placeholder="Email address" required autoFocus onChange={this.onChangeInput.bind(this)}/>
+                    <br />
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Access Game</button>
+                  </form>
                   <p className="mt-5 mb-3 text-muted text-center">
                       ricardo.tercero@r3s.com.mx - &copy; 2018
                   </p>
--- React/mineswipper/src/utils/inputs.js
+++ React/mineswipper/src/utils/inputs.js
@@ -1,4 +1,4 @@
-onChangeInput = (evt, obj) => {
+export const onChangeInput = (evt, obj) => {
   obj.setState({
     [evt.target.name]: evt.target.value
   })
