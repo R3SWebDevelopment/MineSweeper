74248cbf6d048255f2b2d32c66f4b2e608382969
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-05-10 12:09:00 -0500

    Implement grid html template to grid component

--- React/mineswipper/src/components/grid.js
+++ React/mineswipper/src/components/grid.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import '../css/offcanvas.css';
 
 class Grid extends Component{
 
@@ -15,7 +15,134 @@ class Grid extends Component{
 
   render(){
     return(
-      <div />
+      <div>
+        <div class="my-3 p-3 bg-white rounded box-shadow">
+            <h1>
+                Mine Swipper
+            </h1>
+            <h6 class="border-bottom border-gray pb-2 mb-0">Board</h6>
+            <div class="media text-muted pt-3">
+                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <strong class="d-block text-gray-dark">
+                        <a href="#">
+                            Player ricardo.tercero@r3s.com.mx has the turn
+                        </a>
+                    </strong>
+                    Columns: <strong>48</strong>
+                    X
+                    Rows: <strong>24</strong>
+                    &nbsp;
+                    &mdash;
+                    &nbsp;
+                    Mines: <strong>21</strong>
+                    &nbsp;
+                    &mdash;
+                    &nbsp;
+                    Flag Left: <strong>10</strong>
+                    &nbsp;
+                    &mdash;
+                    &nbsp;
+                    Status: <strong>Started</strong>
+                    &nbsp;
+                    &mdash;
+                    &nbsp;
+                    Elapsed Time: <strong>Days (1) Hours (10) Minutes (2) Seconds (1)</strong>
+                </p>
+            </div>
+        </div>
+        <div class="my-3 p-3 bg-white rounded box-shadow">
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                </div>
+            </div>
+        </div>
+        <div class="my-3 p-3 bg-white rounded box-shadow">
+            <div class="media text-muted pt-3">
+                <button type="button" class="btn btn-primary">
+                    Go Back
+                </button>
+                    &nbsp;
+                    &nbsp;
+                <button type="button" class="btn btn-danger">
+                    Pause
+                </button>
+                    &nbsp;
+                    &nbsp;
+                <button type="button" class="btn btn-success">
+                    Resume
+                </button>
+                    &nbsp;
+                    &nbsp;
+                <button type="button" class="btn btn-info">
+                    Restart
+                </button>
+            </div>
+        </div>
+        <p>
+            Nomenclature<br />
+            *     : None Revealed Cell<br />
+            (White Space) : Revealed Cell<br />
+            F     : Flaged Cell<br />
+            B     : Boom<br />
+            [1-8] : Adjacents Count
+        </p>
+      </div>
     )
   }
 }
